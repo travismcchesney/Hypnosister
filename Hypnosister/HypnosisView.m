62b7ffac2818727ccfb3762b8a08a06fb5ccbd23
Travis McChesney <travisj37@hotmail.com>
2012-05-03 21:52:35 -0600

    Changing circle colors

--- Hypnosister/HypnosisView.m
+++ Hypnosister/HypnosisView.m
@@ -10,6 +10,14 @@
 
 @implementation HypnosisView
 
+@synthesize circleColor;
+
+- (void)setCircleColor:(UIColor *)clr
+{
+    circleColor = clr;
+    [self setNeedsDisplay];
+}
+
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
@@ -17,8 +25,23 @@
     if (self){
         // All HypnosisViews start with a clear background color
         [self setBackgroundColor:[UIColor clearColor]];
+        [self setCircleColor:[UIColor lightGrayColor]];
     }
     return self;
+}
+
+- (BOOL)canBecomeFirstResponder
+{
+    return YES;
+}
+
+- (void)motionBegan:(UIEventSubtype)motion withEvent:(UIEvent *)event
+{
+    if (motion == UIEventSubtypeMotionShake) {
+        NSLog(@"Device started shaking!");
+        [self setCircleColor:[UIColor redColor]];
+    }
+
 }
 
 - (void)drawRect:(CGRect)dirtyRect
@@ -32,19 +55,54 @@
     center.y = bounds.origin.y + bounds.size.height / 2.0;
     
     // The radius of the circle should be nearly as big as the view
-    float maxRadius = hypot(bounds.size.width, bounds.size.height) / 4.0;
+    float maxRadius = hypot(bounds.size.width, bounds.size.height) / 2.0;
     
     // The thickness of the line should be 10 points wide
     CGContextSetLineWidth(ctx, 10);
     
-    // The color of the line should be gray (red/green/blue = 0.6, alpha = 1.0);
-    CGContextSetRGBStrokeColor(ctx, 0.6, 0.6, 0.6, 1.0);
+    [[self circleColor] setStroke];
     
-    // Add a shape to the context - this does not draw the shape
-    CGContextAddArc(ctx, center.x, center.y, maxRadius, 0.0, M_PI * 2.0, YES);
+    // Draw concentric circles from the outside in
+    for (float currentRadius = maxRadius; currentRadius > 0; currentRadius -= 20) {
+        // Add a path to the context
+        CGContextAddArc(ctx, center.x, center.y,
+                        currentRadius, 0.0, M_PI * 2, YES);
+        
+        // Perform drawing instruction; rempves path
+        CGContextStrokePath(ctx);
+    }
     
-    // Perform a drawing instruction; draw current shape with current state
-    CGContextStrokePath(ctx);
+    // Create a string
+    NSString *text = @"You are getting sleepy.";
+    
+    // Get a font to draw it in
+    UIFont *font = [UIFont boldSystemFontOfSize:28];
+    
+    CGRect textRect;
+    
+    // How big is this string when drawn in this font?
+    textRect.size = [text sizeWithFont:font];
+    
+    // Let's put that string in the center of the view
+    textRect.origin.x = center.x - textRect.size.width / 2.0;
+    textRect.origin.y = center.y - textRect.size.height / 2.0;
+    
+    // Set the fill color of the current context to black
+    [[UIColor blackColor] setFill];
+    
+    // The shadow will move 4 points to the right and 3 points down from the text
+    CGSize offset = CGSizeMake(4, 3);
+    
+    // The shadow will be dark gray in color
+    CGColorRef color = [[UIColor darkGrayColor] CGColor];
+    
+    // Set the shadow of the context with these parameters,
+    // all subsequent drawing will be shadowed
+    CGContextSetShadowWithColor(ctx, offset, 2.0, color);
+    
+    // Draw the string
+    [text drawInRect:textRect
+            withFont:font];
 }
 
 @end
--- Hypnosister/HypnosisterAppDelegate.m
+++ Hypnosister/HypnosisterAppDelegate.m
@@ -18,17 +18,16 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     
-    CGRect viewFrame = CGRectMake(160, 240, 100, 150);
-    
-    HypnosisView *view = [[HypnosisView alloc] initWithFrame:viewFrame];
+    HypnosisView *view = [[HypnosisView alloc] initWithFrame:[[self window] bounds]];
     
     [[self window] addSubview:view];
     
-    CGRect anotherFrame = CGRectMake(20, 30, 50, 50);
-    
-    HypnosisView *anotherView = [[HypnosisView alloc] initWithFrame:anotherFrame];
-     
-    [view addSubview:anotherView];
+    BOOL success = [view becomeFirstResponder];
+    if (success) {
+        NSLog(@"HypnosisView became the first responder");
+    } else {
+        NSLog(@"Could not become first responder");
+    }
     
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];

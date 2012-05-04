2a56ab46bd2e9db9f2f3ebada9ed0f9f4efd5e4e
Travis McChesney <travisj37@hotmail.com>
2012-05-03 19:55:49 -0600

    Initial Commit

---
+++ Hypnosister/HypnosisterAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HypnosisterAppDelegate.h
+//  Hypnosister
+//
+//  Created by Travis McChesney on 5/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnosisterAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

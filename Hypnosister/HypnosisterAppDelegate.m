90cb64593f619fef7a3a45c4ee716707ecada88a
Travis McChesney <travisj37@hotmail.com>
2012-05-04 19:36:59 -0600

    Added panning and paging

--- Hypnosister/HypnosisterAppDelegate.m
+++ Hypnosister/HypnosisterAppDelegate.m
@@ -18,9 +18,28 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     
-    HypnosisView *view = [[HypnosisView alloc] initWithFrame:[[self window] bounds]];
+    CGRect screenRect = [[self window] bounds];
     
-    [[self window] addSubview:view];
+    // Create the UIScrollView to have the size of the window, matching its size
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:screenRect];
+    [scrollView setPagingEnabled:YES];
+    [[self window] addSubview:scrollView];
+    
+    // Create the HypnosisView with a frame that is twice the size of the screen
+    CGRect bigRect = screenRect;
+    bigRect.size.width *= 2;
+    HypnosisView *view = [[HypnosisView alloc] initWithFrame:screenRect];
+    
+    // Add the HypnosisView as a subview of the scrollView instead of the window
+    [scrollView addSubview:view];
+    
+    // Move the rectangle for the other HypnosisView to the right, just off the screen
+    screenRect.origin.x = screenRect.size.width;
+    HypnosisView *anotherView = [[HypnosisView alloc] initWithFrame:screenRect];
+    [scrollView addSubview:anotherView];
+    
+    // Tell the scrollView how big its virtual world is
+    [scrollView setContentSize:bigRect.size];
     
     BOOL success = [view becomeFirstResponder];
     if (success) {
